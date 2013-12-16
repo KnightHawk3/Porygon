d169743db8fc3228d9416f69fe06a681d4bbc808
Declan Kelly <declan@KnightHawk3.com>
2013-12-16 13:24:38 +0000

    Added colons and removed useless numbers

--- static/porygon.js
+++ static/porygon.js
@@ -8,11 +8,18 @@ function visitObj($container, obj, itterations, ulclass) {
 
     for (var prop in obj) {
         var $li = $('<li>');
-        $li.append('<h'+itterations+' class="name">' + prop + '</h'+itterations+'>');
+        if (itterations != starting_itter) {
+            var colon = ": ";
+        } else {
+            var colon = "";
+        }
+        if (prop != 0) {
+            $li.append('<h'+itterations+' class="name">' + prop + '</h'+itterations+'>'+colon);
+        }
         if (typeof obj[prop] === "object") {
              visitObj($li, obj[prop], itterations + 1, 'json-value');
         } else {
-            $li.append(': <span class="json-object">'+obj[prop]+'</span>');                   
+            $li.append('<span class="json-object">'+obj[prop]+'</span>');
         }
         $ul.append($li);
     }
