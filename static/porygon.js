ce7cd3680cd6bf26836f070fe45ca369120fd61c
Declan Kelly <declan@KnightHawk3.com>
2013-12-17 13:18:43 +0000

    Neater gender bars

--- static/porygon.js
+++ static/porygon.js
@@ -9,11 +9,11 @@ function parsePokedex($container, pokedex, itterations) {
         $li.append('</br><span class="id">Pokedex Number: ' + pokedex[pokemon]["num"] +"</span>")
         $li.append('</br><span class="types">Types: ' + pokedex[pokemon]["types"] +"</span>")
         if (pokedex[pokemon]["gender ratio"]){
-            $li.append('</br>Gender: ' + makebar(pokedex[pokemon]["gender ratio"]["M"]*100, '#3399FF', '#FF99CC'));
+            $li.append('</br>Gender:<div class="gender-container">' + makebar(pokedex[pokemon]["gender ratio"]["M"]*100, '#3399FF', '#FF99CC'));
         } else if (pokedex[pokemon]["gender"]){
-            $li.append('</br>Gender: ' + makebar(100));
+            $li.append('</br>Gender:<div class="gender-container">' + makebar()+ '</div>');
         } else {
-            $li.append('</br>Gender: ' + makebar(50, '#3399FF', '#FF99CC'));
+            $li.append('</br>Gender:<div class="gender-container">' + makebar(50, '#3399FF', '#FF99CC')+ '</div>');
         }
         $ul.append($li); // Add the item to the list
     }
@@ -22,9 +22,9 @@ function parsePokedex($container, pokedex, itterations) {
 
 function makebar(percent, color1, color2) {
     if (color1 && color2) {
-        var background = '<div class="progress gender" style="background: -o-linear-gradient(left, '+ color1 +' '+percent+'%, '+ color2 +' 0%); background: -moz-linear-gradient(left, '+ color1 +' '+percent+'%, '+ color2 +' 0%); background: -webkit-linear-gradient(left, '+ color1 +' '+percent+'%, '+ color2 +' 0%); background: linear-gradient(left, '+ color1 +' '+percent+'%, '+ color2 +' 0%); background: -ms-linear-gradient(left, '+ color1 +' '+percent+'%, '+ color2 +' 0%);"></div>';
+        var background = percent + '% <div class="progress gender" style="background: -o-linear-gradient(left, '+ color1 +' '+percent+'%, '+ color2 +' 0%); background: -moz-linear-gradient(left, '+ color1 +' '+percent+'%, '+ color2 +' 0%); background: -webkit-linear-gradient(left, '+ color1 +' '+percent+'%, '+ color2 +' 0%); background: linear-gradient(left, '+ color1 +' '+percent+'%, '+ color2 +' 0%); background: -ms-linear-gradient(left, '+ color1 +' '+percent+'%, '+ color2 +' 0%);"></div>'+(100-percent)+'%';
     } else {
-        var background = '<div class="progress gender"><center>Genderless</center></div>';
+        var background = '<div class="progress gender genderless">Genderless</div>';
     }
     return background;
 }
