d4c3adb88d2283fe84dd5b1bf94a46283fe01b21
Declan Kelly <declan@KnightHawk3.com>
2013-12-17 05:45:35 +0000

    Added option to sort by id and displays id's

--- static/porygon.js
+++ static/porygon.js
@@ -10,6 +10,7 @@ function parsePokedex($container, pokedex, itterations, ulclass) {
         var $li = $('<li>'); // Start the item for the pokemon
         $li.append('<h'+itterations+' class="name">' + pokemon + '</h'+itterations+'>'); // Print the pokemon
         //parsePokedex($li, pokedex[pokemon], itterations + 1, 'json-value');
+        $li.append('</br><span class="id">Pokedex Number: ' + pokedex[pokemon]["num"] +"</span>")
         $ul.append($li); // Add the item to the list
     }
     $container.append($ul); // Add the list to the div
