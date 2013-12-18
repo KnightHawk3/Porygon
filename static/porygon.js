a44e85d3e5ba554b0b7581cfcff56ee9e58b0f42
Declan Kelly <declan@KnightHawk3.com>
2013-12-18 08:52:24 +0000

    Neated margins more

--- static/porygon.js
+++ static/porygon.js
@@ -3,7 +3,7 @@ function parsePokedex($container, pokedex, itterations) {
     var $ul = $('<ul class="list list-inline">');
 
     for (var pokemon in pokedex) {
-        var $li = $('<li>'); // Start the item for the pokemon
+        var $li = $('<li class="pokemon-card">'); // Start the item for the pokemon
         $li.append('<h'+itterations+' class="name">' + pokemon + '</h'+itterations+'>'); // Print the pokemon
         //parsePokedex($li, pokedex[pokemon], itterations + 1, 'json-value');
         $li.append('<div class="id">Pokedex Number: ' + pokedex[pokemon]["num"] +"</div>")
